27787f37d1ee71fe6400f07d3f5e4614805c5de2
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-06-25 22:58:33 -0400

    Fixed routes and added a Header component.

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<h1>Home</h1>
+
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, suscipit accusamus, molestiae nemo cumque labore,
+			modi praesentium non fugiat asperiores ut dolore. Fugit voluptatibus voluptates, molestias labore voluptate et
+			commodi?</p>
+	</div>
+</template>
