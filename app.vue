caa43f1b7aaf8981e1f7c8366b960fe98a7440b4
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-06-25 22:59:22 -0400

    Actually... Fixed routes and added a Header component.

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div>
+		<Header />
+
+		<NuxtPage />
+	</div>
 </template>
---
+++ components/header.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<header class="header">
+			<div class="container">
+				<nav class="header__nav">
+					<ul>
+						<li>
+							<NuxtLink to="/">Home</NuxtLink> |
+						</li>
+						<li>
+							<NuxtLink to="/about">About</NuxtLink> |
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	</div>
+</template>
+
+<style scoped>
+	.header {
+		background-color: #111555;
+		color: #fff;
+
+		&__nav {
+			ul {
+				list-style: none;
+				padding: 0;
+				margin: 0;
+				display: flex;
+				justify-content: flex-end;
+				li {
+					a {
+						color: #fff;
+						text-decoration: none;
+					}
+				}
+			}
+
+		}
+	}
+</style>
