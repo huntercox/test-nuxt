b28c069c2dc250f4f5d80ed376c040ae8a9d15d4
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-06-27 22:34:58 -0400

    Add Projects page and move the request/data projects loop from the index page, to the Projects page.

--- components/header.vue
+++ components/header.vue
@@ -10,6 +10,9 @@
 					<li>
 						<NuxtLink to="/about">About</NuxtLink>
 					</li>
+					<li>
+						<NuxtLink to="/projects">Projects</NuxtLink>
+					</li>
 				</ul>
 			</nav>
 		</div>
---
+++ pages/projects.vue
@@ -0,0 +1,61 @@
+<template>
+	<div>
+		<div class="container">
+			<h1>Projects</h1>
+
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, suscipit accusamus, molestiae nemo cumque
+				labore,
+				modi praesentium non fugiat asperiores ut dolore. Fugit voluptatibus voluptates, molestias labore voluptate et
+				commodi?</p>
+
+			<!-- <pre>{{ data }}</pre>> -->
+			<div class="projects__wrap">
+				<h2>My Projects:</h2>
+				<ul class="projects">
+					<Project v-for="project in data" :key="project.uri" :project="project" />
+				</ul>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const config = useRuntimeConfig();
+const { data, refresh, pending } = await useFetch(config.public.wordpressUrl, {
+	method: 'get',
+	query: {
+		query: `
+     query NewQuery {
+			projects(where: {status: PUBLISH}) {
+				nodes {
+					title
+					uri
+					projectId
+					projectDescription
+					projectUrl
+					projectDate
+					projectEmployer
+				}
+			}
+     }`
+	},
+	transform(data) {
+		return data.data.projects.nodes as Array<Record<'title' | 'uri' | 'projectId' | 'projectDescription' | 'projectUrl' | 'projectDate' | 'projectEmployer', string>>;
+	}
+});
+</script>
+
+<style scoped lang="scss">
+.projects {
+	display: flex;
+	justify-content: space-around;
+	align-items: flex-start;
+	margin: 20px 0;
+	list-style-type: none;
+
+	.project {
+		width: 33%;
+	}
+}
+</style>
